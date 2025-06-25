f46a1ef3e34b6d0f5228acc261927589622aa163
CondorianO <nscabuenas@philiino.com>
2025-06-25 08:33:01 +0800

    1st

--- src/components/SideBarComponent.jsx
+++ src/components/SideBarComponent.jsx
@@ -21,6 +21,7 @@ import {
   CardTravelOutlined,
   TrendingUpOutlined,
   PeopleAltOutlined,
+  ListAltOutlined,
 } from "@mui/icons-material";
 import { Link, NavLink, useLocation, useNavigate } from "react-router-dom";
 
@@ -70,6 +71,19 @@ export default function SideBarComponent() {
       title: "Settings",
       component: <SettingsOutlined fontSize="medium" color="primary" />,
     },
+    {
+      title: "Masterlist",
+      component: <ListAltOutlined fontSize="medium" color="primary" />,
+      submenu: [
+        { title: "Business Partner", path: "" },
+        { title: "Company Profile", path: "" },
+        { title: "Customer Master", path: "" },
+        { title: "Item Master", path: "" },
+        { title: "Employee Master", path: "" },
+        { title: "Warehouse Master", path: "" },
+        { title: "Price Master", path: "" },
+      ],
+    }
   ];
   const [selected, setSelected] = useState(0);
   const handlSelectedComponent = (event, index) => {
@@ -77,48 +91,77 @@ export default function SideBarComponent() {
   };
   return (
     <>
-      <List>
-        {sideBarComponent.map((comp, index) => (
-          <ListItem disablePadding dense={true} key={index}>
-            <Box width="100%">
-              <ListItemButton
-                onClick={(event) => {
-                  handlSelectedComponent(event, index);
-                  navigateTo(comp.title.toLocaleLowerCase());
-                }}
-                // selected={}
-                selected={
-                  index === selected &&
-                  currentPage === "/" + comp.title.toLowerCase()
-                }
-                sx={{
-                  mb: 3,
-                  borderLeft: 0,
-                  borderColor: "primary.main",
-                  ml: 1,
-                }}
-              >
-                <ListItemIcon>
-                  <IconButton>{comp.component}</IconButton>
-                </ListItemIcon>
-                {/* <Link
-                  to={"" + comp.title.toLocaleLowerCase()}
-                  className="router-link"
-                > */}
-                <ListItemText
-                  primary={comp.title}
-                  primaryTypographyProps={{
-                    fontSize: "medium",
-                    fontWeight: selected === index ? "bold" : "",
-                    color: selected === index ? "primary.main" : "inherit",
+    <List>
+      {sideBarComponent.map((comp, index) => {
+        const hasSubmenu = !!comp.submenu;
+        const isSelected = index === selected && currentPage.includes(comp.title.toLowerCase());
+        const [openSubmenu, setOpenSubmenu] = useState(false);
+
+        return (
+          <React.Fragment key={index}>
+            <ListItem disablePadding dense>
+              <Box width="100%">
+                <ListItemButton
+                  onClick={(event) => {
+                    handlSelectedComponent(event, index);
+                    if (hasSubmenu) {
+                      setOpenSubmenu(!openSubmenu); // toggle submenu
+                    } else {
+                      navigateTo(comp.title.toLowerCase());
+                    }
                   }}
-                />
-                {/* </Link> */}
-              </ListItemButton>
-            </Box>
-          </ListItem>
-        ))}
-      </List>
+                  selected={isSelected}
+                  sx={{
+                    mb: 1,
+                    borderLeft: 0,
+                    borderColor: "primary.main",
+                    ml: 1,
+                  }}
+                >
+                  <ListItemIcon>
+                    <IconButton>{comp.component}</IconButton>
+                  </ListItemIcon>
+                  <ListItemText
+                    primary={comp.title}
+                    primaryTypographyProps={{
+                      fontSize: "medium",
+                      fontWeight: isSelected ? "bold" : "",
+                      color: isSelected ? "primary.main" : "inherit",
+                    }}
+                  />
+                </ListItemButton>
+              </Box>
+            </ListItem>
+
+            {/* Submenu Rendering */}
+            {hasSubmenu && openSubmenu && (
+              <Box ml={6}>
+                {comp.submenu.map((item, subIndex) => (
+                  <ListItemButton
+                    key={subIndex}
+                    sx={{ pl: 2 }}
+                    onClick={() => navigateTo(`/${item.path}`)}
+                    selected={currentPage === "/" + item.path}
+                  >
+                    <ListItemText
+                      primary={item.title}
+                      primaryTypographyProps={{
+                        fontSize: "small",
+                        color:
+                          currentPage === "/" + item.path
+                            ? "primary.main"
+                            : "inherit",
+                      }}
+                    />
+                  </ListItemButton>
+                ))}
+              </Box>
+            )}
+          </React.Fragment>
+        );
+      })}
+    </List>
+
       {/* <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
         <Alert onClose={handleClose} severity="success" sx={{ width: '100%' }}>
           This is a success message!
